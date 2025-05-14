e50005fb1841891906398b52559d1ccd25f4c997
trilokluniya27 <trilokluniya27@gmail.com>
2025-05-14 09:57:16 +0000

    datatype

---
+++ 01_tutorial/datatypes.js
@@ -0,0 +1,7 @@
+
+
+// Number age = 25
+// String name = "trilok"
+// Boolean true and false
+// undefined  undefined
+// null  Object
--- 01_tutorial/tutorial.js
+++ 01_tutorial/tutorial.js
@@ -1,1 +1,19 @@
-console.log("trilok luniya")
+
+// varible 
+
+// const userId = 10001;
+// const firstName = "Trilok";
+// const lastName = "Luniya";
+// const dateOfBirth = 25;
+// const mobileNo  =   8224054220;
+// const city  = "Dewas";
+// const state = "Madhya Pradesh"
+
+// console.log(userId);
+// console.log(firstName+ " " +lastName);
+// console.log(dateOfBirth);
+// console.log(mobileNo);
+// console.log(city);
+// console.log(state);
+
+// console.table([userId,firstName+" "+lastName,dateOfBirth,mobileNo,city,state]);
